da15cadcfe734a30c6d83f7dc233496d1059947a
notcelsius <149901647+notcelsius@users.noreply.github.com>
2024-12-29 13:48:10 -0800

    We're so back

---
+++ Solutions/138. Copy List with Random Pointer.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (head == NULL) { // if list is empty new list will be empty
+            return head;
+        }
+        unordered_map<Node*,Node*> origToNew;   // stores original nodes to their deep copies
+        Node* curr = head;
+        // create new copies of all the nodes
+        while (curr) {
+            origToNew[curr] = new Node(curr->val);
+            curr = curr->next;
+        }
+        curr = head;
+        // set the random and next ptrs for the deep copies
+        while (curr) {
+            origToNew[curr]->next = origToNew[curr->next];
+            origToNew[curr]->random = origToNew[curr->random];
+            curr = curr->next;       
+        }
+        // return head of new list
+        return origToNew[head];
+        
+    }
+};
